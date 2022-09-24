156f8bb4d6697a0e1e284ce3ec88bfc6b40daf94
YAHIA ABDCHAFEE ADAM <YAHIA ABDCHAFEE Adam>
2022-09-24 23:25:37 +0200

    edit auto

--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2');
-const connect = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    waitForConnections: true,
-});
-module.exports = connect;
